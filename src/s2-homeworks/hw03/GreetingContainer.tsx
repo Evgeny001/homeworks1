e7a8810b635a625aad7c8c00f975023ca1f8e8e6
EvgenyLukashevich <info.elukas@gmail.com>
2023-03-23 11:00:16 +0300

    fixed_in_homework_3

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,7 +1,6 @@
 import React, { ChangeEvent, KeyboardEvent, useState, MouseEvent } from 'react'
 import Greeting from './Greeting'
 import { UserType } from './HW3'
-// import any = jasmine.any;
 import {Simulate} from "react-dom/test-utils";
 import error = Simulate.error;
 
@@ -20,17 +19,6 @@ export const pureAddUser = (name: string, setError: (error:string)=> void, setNa
     }
     // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
 }
-
-// export const pureAddUser = (name: string, setError: (error: string) => void, setName: (name: string) => void, addUserCallback: (name: string) => void) => {
-//
-//     if (!name.trim()) {
-//         setError('Ошибка! Введите имя!')
-//     } else {
-//         addUserCallback(name)
-//         setName('')
-//     }
-//     // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
-// }
 
 
 export const pureOnBlur = (name: string, setError: (error:string) =>void) => { // если имя пустое - показать ошибку
@@ -60,7 +48,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
-        setName(name) // need to fix
+        setName(e.currentTarget.value) // need to fix
 
         error && setError('')
     }
